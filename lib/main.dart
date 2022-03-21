67e8203bacf553bcf9b87504c56cb0ba72399d49
datapirates0101 <datapirates0101@gmail.com>
2022-03-21 13:54:13 +0530

    added login screen and conncted to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:zoom_clone/screens/screens.dart';
+import 'package:zoom_clone/screens/home_screen.dart';
+import 'package:zoom_clone/screens/login_screen.dart';
 import 'package:zoom_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Zoom Clone',
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: backgroundColor),
-      home: const LoginScreen(),
+      theme:
+          ThemeData.dark().copyWith(scaffoldBackgroundColor: backgroundColor),
+      routes: {
+        '/' :(context) => LoginScreen(),
+        'home':(context) => HomeScreen(),
+      },
     );
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/resources/atuh_methods.dart';
 import 'package:zoom_clone/widgets/custom_button.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -9,6 +10,7 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  AuthMethods _authMethods = AuthMethods();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +30,15 @@ class _LoginScreenState extends State<LoginScreen> {
               padding: const EdgeInsets.symmetric(vertical: 38),
               child: Image.asset('assets/images/onboarding.jpeg'),
             ),
-            CustomButton(text: 'Login',onPressed: (){},)
+            CustomButton(
+              text: 'Login',
+              onPressed: () async {
+                bool res = await _authMethods.signUpWithGoogle(context);
+                if (res == true) {
+                  Navigator.pushNamed(context, 'home');
+                }
+              },
+            )
           ],
         ),
       ),
