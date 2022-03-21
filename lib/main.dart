986adb7875411e313d9f4991c7bd2ed2fc2fff86
datapirates0101 <datapirates0101@gmail.com>
2022-03-21 11:07:39 +0530

    added login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/screens/screens.dart';
+import 'package:zoom_clone/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter demo',
-      theme:  ThemeData(primarySwatch: Colors.blue),
-      home: Text('Hello world'),
+      title: 'Zoom Clone',
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: backgroundColor),
+      home: const LoginScreen(),
     );
   }
 }
